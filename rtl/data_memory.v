740a456d294394f983100f50af6bfb50146a0a00
pritha-nandy <prithanandy.6002@gmail.com>
2026-08-29 12:45:03 +0530

    Add data memory module with read/write functionality
    Implemented a data memory module with read and write capabilities.

--- rtl/data_memory.v
+++ rtl/data_memory.v
@@ -1,1 +1,23 @@
+`timescale 1ns / 1ps
+module data_memory(input [31:0]address,input clk,input Memwrite,output reg [31:0]read_data,input [31:0] write_data
+
+    );
+    reg[31:0]data_mem[0:255];
+    integer i;
+    initial begin
+        for(i=0;i<256;i=i+1)
+             data_mem[i]=32'b0;
+    end
+    always @(*)
+        begin
+            read_data=data_mem[address>>2];
+        end
+    always @(posedge clk)
+        begin
+            if(Memwrite)
+                data_mem[address>>2]<=write_data;
+        end
+        
+    
+endmodule
 
