15e2d5f6f98979d7bd46755cb31278581fc0d010
pritha-nandy <prithanandy.6002@gmail.com>
2026-08-29 12:41:11 +0530

    Implement immediate generator module for instruction types

--- rtl/immediate_generator.v
+++ rtl/immediate_generator.v
@@ -1,1 +1,17 @@
+`timescale 1ns / 1ps
+module immediate_generator( input[2:0]imm_src,input [31:0] instruction,output reg[31:0]imm_ext
+
+    );
+    always @(*)
+        begin
+            case(imm_src)
+                3'b000: imm_ext={{20{instruction[31]}},instruction[31:20]};//I-type
+                3'b001: imm_ext={{20{instruction[31]}},instruction[31:25],instruction[11:7]};//S-type
+                3'b010: imm_ext={{19{instruction[31]}},instruction[31],instruction[7],instruction[30:25],instruction[11:8],1'b0};//B-type
+                3'b011: imm_ext={instruction[31:12],{12{1'b0}}};//U-type
+                3'b100: imm_ext={{11{instruction[31]}},instruction[31],instruction[19:12],instruction[20],instruction[30:21],1'b0};//J type
+                default: imm_ext=32'b0;
+            endcase
+        end
+endmodule
 
